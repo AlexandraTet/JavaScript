4eadff58a8db828faefc44600ac05a68de25bdeb
Alexandra Tetiorkina <atrobber@gmail.com>
2026-05-07 11:47:49 +0300

    Add files via upload

---
+++ JS_Lesson1/script/js.js
@@ -0,0 +1,65 @@
+// // // // // document.write("Hello JS!");
+// // // // console.log("Hello JS!");
+// // // // console.info("Hello World!");
+// // // // console.error("No World!");
+// // // // console.warn("Where is the World?");
+// // //
+// // // var MyFirstVar;
+// // // myFirstVar = 15;
+// // // console.log(myFirstVar);
+// // //
+// // // var mySecondVar = "sheep!";
+// // // console.log(myFirstVar + " " + mySecondVar);
+// // // console.log(MyFirstVar - mySecondVar);
+// // //
+// // // var int = 10;
+// // // var float = 10.5;
+// // // var string = "9";
+// // // var boolean = true;
+// // // var bool = false;
+// // // var a;
+// // // a = null;
+// // // console.log(a);
+// // //
+// // // let b = 10, a = b +2;
+// //
+// //
+// // var a = 10;
+// // var a = 20;
+// // console.log(a);
+// //
+// // const PI = 3.14;
+// //
+// // let a = 10;
+// // let b = 3;
+// // console.log(a + b);
+// // console.log(a - b);
+// // console.log(a * b);
+// // console.log(a / b);
+// // console.log(a % b);
+// // console.log(a ** b);
+// //
+// // // a = 2 + 2;
+// // // b = a + 10;
+// // //
+// // // a = a + 2;
+// // // a += 2;
+// //
+// // // a++; // sufix ++ - increment
+// // // a--; // sufix -- - decrement
+// // //
+// // // --a; // postfix -- - decrement
+// // // ++a; // prefix ++ - increment
+// //
+// // let a = 2, b;
+// // b = a++;
+// // console.log(b);
+//
+// let a = 3, b = 5;
+// console.log((a - 0) + (b - 0));
+// console.log(Number(a) - Number(b));
+//
+// interpolation
+
+let name = "John", age = 30;
+console.log(`Hello ${name}, you are ${age} years old!`);
