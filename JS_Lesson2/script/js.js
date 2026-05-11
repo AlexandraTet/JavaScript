f726cfaba19b24405604e933a94d99ebd90f947b
Alexandra Tetiorkina <atrobber@gmail.com>
2026-05-11 10:01:30 +0300

    Add files via upload

---
+++ JS_Lesson2/script/js.js
@@ -0,0 +1,109 @@
+// // // // if (example){
+// // // //     [instructial];
+// // // //     [instructial];
+// // // // }
+// // //
+// // // // 0n - big integer
+// // // // 0n, '', o, null, NaN, false - false
+// // //
+// // // // оператори порівняння
+// // // // let a = 2, b = 3, c = '3';
+// // // // console.log(a == b);
+// // // // console.log(a != b);
+// // // // console.log(a > b); //< <= >=
+// // // //
+// // // // console.log(b === c); !==
+// // //
+// // // let a = 2, b = 3, c;
+// // //
+// // // // if (a > b){
+// // // //     c = 'a > b'
+// // // // } else{
+// // // //     c = 'a < b'
+// // // // }
+// // // // console.log(c);
+// // //
+// // // // if (a > b){
+// // // //     c = 'a > b'
+// // // // } else{
+// // // //     if (a < b){
+// // // //         c = 'a < b'
+// // // //     }
+// // // //     else{
+// // // //         c = 'a == b'
+// // // //     }
+// // // // }
+// // //
+// // // // if (a > b){
+// // // //     c = 'a > b'
+// // // // } else if (a < b){
+// // // //     c = 'a < b'
+// // // // } else{
+// // // //     c = 'a == b'
+// // // // }
+// // //
+// // // // if (a > b)c = 'a > b';
+// // // // else if (a < b)c = 'a < b';
+// // // // else c = 'a === b';
+// // //
+// // //
+// // //
+// // //
+// // // // switch case
+// // // let  course, title;
+// // // course = prompt('Enter course name: ');
+// // //
+// // // switch (course){
+// // //     case 'javascript':
+// // //         title = 'JavaScript code language';
+// // //         break;
+// // //
+// // //     case 'html':
+// // //         title = 'HTML code language';
+// // //         break;
+// // //
+// // //     case 'Frontend':
+// // //     case 'HTML':
+// // //     case 'CSS':
+// // //         title = 'HTML + CSS code language';
+// // //         break;
+// // //
+// // //     default:
+// // //         title = 'Unknown course';
+// // //         break;
+// // // }
+// // //
+// // //
+// // // // console.log(title);
+// // // alert(title);
+// // //
+// // //
+// // // logical operators
+// // // let a = false;
+// // // console.log(!a); //denial
+// // //
+// // // let b = 'text';
+// // // console.log(!!b);
+// //
+// // // logical I
+// // let a = true, b = true, c = false;
+// // console.log(a && b); //conditional: true %% true = true. true %% false = false
+// // console.log(a && c);
+// //
+// // //logical or
+// // console.log(b || c);
+//
+// let age = prompt('Enter your age: '), info;
+// if (age >= 18 && age <= 35){
+//     info = 'Person`s age is between 18 and 35'
+// }
+// else if (age > 35){
+//     info = 'Person`s age is more than 35'
+// }
+// else{
+//     info = 'Person`s age is less than 18'
+// }
+//
+// alert(info);
+
+
